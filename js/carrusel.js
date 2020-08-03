7245c1a6ef63f0dd6f72a3409d07a6cc2bbb5fe8
Harold Andrés Bartolo Moscoso <habartolom@unal.edu.co>
2020-08-03 00:05:15 -0500

    Arreglo Carrusel 8 imagenes

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,5 +1,4 @@
 const descripcion = document.getElementById("descripcion");
-const imagen1 = document.getElementById("imagen1");
 const imagen2 = document.getElementById("imagen2");
 const imagen3 = document.getElementById("imagen3");
 const imagen4 = document.getElementById("imagen4");
@@ -21,7 +20,6 @@ const descripciones = [
     "<div>¿Sufres de dolores en tu Túnel Carpiano?  <br> <span>Nosotros te ayudamos<span></div>"
 ]
 
-imagen1.addEventListener("mouseover", escribirDescripcion1);
 imagen2.addEventListener("mouseover", escribirDescripcion2);
 imagen3.addEventListener("mouseover", escribirDescripcion3);
 imagen4.addEventListener("mouseover", escribirDescripcion4);
@@ -30,7 +28,6 @@ imagen6.addEventListener("mouseover", escribirDescripcion6);
 imagen7.addEventListener("mouseover", escribirDescripcion7);
 imagen8.addEventListener("mouseover", escribirDescripcion8);
 imagen9.addEventListener("mouseover", escribirDescripcion9);
-imagen1.addEventListener("mouseleave", borrarDescripcion);
 imagen2.addEventListener("mouseleave", borrarDescripcion);
 imagen3.addEventListener("mouseleave", borrarDescripcion);
 imagen4.addEventListener("mouseleave", borrarDescripcion);
@@ -40,9 +37,6 @@ imagen7.addEventListener("mouseleave", borrarDescripcion);
 imagen8.addEventListener("mouseleave", borrarDescripcion);
 imagen9.addEventListener("mouseleave", borrarDescripcion);
 
-function escribirDescripcion1(){
-    descripcion.innerHTML = '<div class="malestar">¿Sufres de Dolor de Rodilla?  </div><div class="consejo">Contáctanos para ayudarte!</div>';
-}
 function escribirDescripcion2(){
     descripcion.innerHTML = '<div class="malestar">¿Sufres de Fuertes Dolores de Cuello?  </div><div class="consejo">Contáctanos para solicitar una cita con nosotros</div>';
 }
